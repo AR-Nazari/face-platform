79f4a2cb1905d491001d03ee509243ccb39e4a90
Ali Reza Nazari <Ali.Reza.Nazari@Gmail.com>
2025-12-06 03:02:36 +0330

    er

---
+++ src/dotnet/backend/Face.Infrastructure/Persistence/Migrations/20251205231605_AddUserPreferredLanguage.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Face.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserPreferredLanguage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PreferredLanguage",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "PreferredLanguage",
+                value: "fa-IR");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "PreferredLanguage",
+                value: "fa-IR");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PreferredLanguage",
+                table: "Users");
+        }
+    }
+}
